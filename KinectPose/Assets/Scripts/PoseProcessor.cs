fbce7856e69a01ae1bafc9b3ba80e7a45a5e47b3
tarcisio <tcosta@ucsc.edu>
2015-07-18 18:39:38 -0700

    Scene cleanup and new script

---
+++ KinectPose/Assets/Scripts/PoseProcessor.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoseProcessor : MonoBehaviour 
+{
+	[Header("References")]
+	public GUITexture backgroundImage;
+	public GameObject OverlayObject;
+	public GUIText debugText;
+
+	[Header("Options")]
+	public float smoothFactor = 5f;
+
+	private float distanceToCamera = 10f;
+
+	KinectManager manager;
+
+	KinectWrapper.NuiSkeletonPositionIndex TrackedJoint = KinectWrapper.NuiSkeletonPositionIndex.HandRight;
+	KinectWrapper.NuiSkeletonPositionIndex HandLeft = KinectWrapper.NuiSkeletonPositionIndex.HandLeft;
+
+	KinectWrapper.NuiSkeletonPositionIndex FootLeft = KinectWrapper.NuiSkeletonPositionIndex.FootLeft;
+	KinectWrapper.NuiSkeletonPositionIndex FootRight = KinectWrapper.NuiSkeletonPositionIndex.FootRight;
+
+	void Start()
+	{
+		manager = KinectManager.Instance;
+
+		if(OverlayObject)
+		{
+			distanceToCamera = (OverlayObject.transform.position - Camera.main.transform.position).magnitude;
+		}
+	}
+
+
+	void Update() 
+	{
+		if(manager && manager.IsInitialized())
+		{
+			if(backgroundImage && (backgroundImage.texture == null))
+			{
+				backgroundImage.texture = manager.GetUsersClrTex();
+			}
+
+			int iJointIndex = (int)TrackedJoint;
+
+			if(manager.IsUserDetected())
+			{
+				uint userId = manager.GetPlayer1ID();
+
+				if(manager.IsJointTracked(userId, iJointIndex))
+				{
+					Vector3 posJoint = manager.GetRawSkeletonJointPos(userId, iJointIndex);
+
+					if(posJoint != Vector3.zero)
+					{
+						// 3d position to depth
+						Vector2 posDepth = manager.GetDepthMapPosForJointPos(posJoint);
+
+						// depth pos to color pos
+						Vector2 posColor = manager.GetColorMapPosForDepthPos(posDepth);
+
+						float scaleX = (float)posColor.x / KinectWrapper.Constants.ColorImageWidth;
+						float scaleY = 1.0f - (float)posColor.y / KinectWrapper.Constants.ColorImageHeight;
+
+						if(debugText)
+						{
+							debugText.GetComponent<GUIText>().text = "Tracked user ID: " + userId;  // new Vector2(scaleX, scaleY).ToString();
+						}
+
+						if(OverlayObject)
+						{
+							Vector3 vPosOverlay = Camera.main.ViewportToWorldPoint(new Vector3(scaleX, scaleY, distanceToCamera));
+							OverlayObject.transform.position = Vector3.Lerp(OverlayObject.transform.position, vPosOverlay, smoothFactor * Time.deltaTime);
+						}
+					}
+				}
+
+			}
+
+		} // if manager init
+	}
+
+
+
+
+
+
+}
